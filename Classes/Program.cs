0a4a1a05d10709ee632cdbf3cb427602919c4d8e
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-10-09 12:13:13 +0100

    Added readme and completed Classes YouTube tuorial

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -6,12 +6,45 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
+    // basic class without getters and setters
+    public class Person
+    {
+        public string Name;
+        public void Introduce(string to)
+        {
+            // format string with parameters that we will fill in this order
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string myString)
+        {
+            var person = new Person();
+            person.Name = myString;
+            return person;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            /* Using the istance method Introduce means we have to create an instance of the Person
+             */ 
+            var person = new Person();
+            person.Name = "Evie";
+            person.Introduce("Mosh");
+            
+            /* Using thr static method Parse. It's static because if it were an instance method, 
+             * we'd have to creaate an instance of Person
+            before calling the Parse method which returns a Person. Doesn't make sense to create 
+            *a Person twice! so here we can call Parse on the class not the object
+            */
+          var nelleth = Person.Parse("Nelly");
+            //Console.WriteLine(nelleth);    you will see this is the class Person
+
+            //we get the same result
+            nelleth.Introduce("Dr Who");
+          Console.ReadKey();
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+# Programming with Mosh
+
+## Mosh is a legend and I love his tutorials!
+
+
+
+Mosh's YouTube channel [https://www.youtube.com/channel/UCWv7vMbMWH4-V0ZXdmDpPBA]
