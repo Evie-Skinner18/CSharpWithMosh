3dc39e79cb4a83f39ff313e4d47ef2e3672ef652
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-10-09 13:56:03 +0100

    Added an Order class and loads of comments to Customer.cs

--- Constructors/Customer.cs
+++ Constructors/Customer.cs
@@ -1,12 +1,19 @@
-﻿namespace Constructors
+﻿using System.Collections.Generic;
+
+namespace Constructors
 {
-    // a ctor is a method that is called when an instance of a class is created (it's like initialize)
+    /*a ctor is a method that is called when an instance of a class is created (it's like initialize)
+     also like initialize, a ctor is not essential to a class. You only want a ctor method if
+     you need the class to have something particular early on. you don't always need to initialise
+     an object at an early state!*/
     public class Customer
     {
 
         // public is not best way to declare fields but we'll see betterway when we get to Properties
         public int Id;
         public string Name;
+        // in this List we can store objects of type Order
+        public List<Order> Orders;
 
         /* we can overload the class by having more than one ctor! This means the way that we
         instantiate an object can be flexible. We have three ctors here so can create a Customer
@@ -29,4 +36,5 @@
 
         }
     } // end of Customer class
+
 }
