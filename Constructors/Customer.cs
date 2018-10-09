7d441d4bb7c059c26d39358268c037c35003ebc6
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-10-09 12:33:32 +0100

    Started the constructors tutorial

---
+++ Constructors/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Constructors
+{
+    // a ctor is a method that is called when an instance of a class is created (it's like initialize)
+    public class Customer
+    {
+        // public is not best way to declare fields but we'll see betterway when we get to Properties
+        public int Id;
+        public string Name;
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            //Console.WriteLine("Hello World!");
+
+
+            Console.ReadKey();
+        }
+    }
+}
