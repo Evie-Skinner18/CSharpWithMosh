9b9bb6948ffaba44306dad75be3a18e321f38bcf
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-10-10 11:51:05 +0100

    Added exception in Point.cs to handle the situation if a null argument is passed to Move method

--- Methods/Point.cs
+++ Methods/Point.cs
@@ -1,4 +1,6 @@
-﻿namespace Methods
+﻿using System;
+
+namespace Methods
 {
     public class Point
     {
@@ -11,7 +13,7 @@
             this.Y = y;
         }
 
-        //one Move method
+        //one Move method that takes just an x and y. This is FIRST OVERLOAD
         public void Move(int x, int y)
         {
             this.X = x;
@@ -19,11 +21,23 @@
         }
 
         // we can overload the Movemethod by making another one that takes a different signature,
-        // like a Point object
+        // like a Point object. So now we have two slightly different ways (OVERLOADS) of invoking the method
         public void Move(Point newLocation)
         {
+            /* Don't need to repeat orselves here if we make this Move method invoke the
+             * Move method above!
+             * 
             this.X = newLocation.X;
             this.Y = newLocation.Y;
+             **/
+
+            // if the argument is null, the code will never reach this bit. So te Point object
+            // will always bein a valid state. Validating the argument
+            if (newLocation == null)
+                throw new ArgumentNullException("newLocation");
+
+            
+            Move(newLocation.X, newLocation.Y);
         }
     }
 }
--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,12 +10,27 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            var mapPoint = new Point(0, 10);
-            mapPoint.Move(new Point(40, 60));
-            Console.WriteLine("mapPoint is now at ({0}, {1})", mapPoint.X, mapPoint.Y);
+            try
+            {
+                // here we make a Point object and call the second version of the Move method. It takes
+                // a Point object as an argument. moves one Point to a whole new Point
+                var mapPoint = new Point(0, 10);
+                // if the argument you pass to Move is null, it willthrow an exception referencing newLocation
+                mapPoint.Move(new Point(20, 60));
+                Console.WriteLine("Version 2 of the Move method: mapPoint is now at ({0}, {1})", mapPoint.X, mapPoint.Y);
 
-        
-            Console.ReadKey();
+                // now we use the first version of the Move method that just takes an x and y
+                mapPoint.Move(20, 0);
+                Console.WriteLine("Version 1 of the Move method: mapPoint is now at ({0}, {1})", mapPoint.X, mapPoint.Y);
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+
+                throw;
+                //Console.ReadKey();
+            }
+            
         }
     }
 }
