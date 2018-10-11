fa4d3df57da26127802a4b3e672af8b95028a69f
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-10-11 13:17:32 +0100

    Added a private field, getter and setter method to access it

--- AccessModifiers/Person.cs
+++ AccessModifiers/Person.cs
@@ -1,10 +1,26 @@
-﻿namespace AccessModifiers
+﻿using System;
+
+namespace AccessModifiers
 {
     partial class Program
     {
         public class Person
         {
+            // if the name is private, you cannot access the name elsewjere: can't put john.Name
+            private DateTime _birthdate;
 
+            // setter method gives birthdate a value
+            public void SetBirthdate(DateTime birthdate)
+            {
+                _birthdate = birthdate;
+            }
+            // getter method goes and reads that value
+            public DateTime GetBirthdate()
+            {
+                return _birthdate;
+            }
+
+        
         }
     }
 }
--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -11,9 +11,21 @@ namespace AccessModifiers
         static void Main(string[] args)
         {
             /* ENCAPSULATION.
-             * Thing about hiding some info and the chef and the waitress
+             * We can hide some information. The waitress doesn't go and tell the chef how to chop the
+             * veg, chop the meat etc. Similarly, the chef doesnt tell the waitress how to clear the tables
+             * or be all charming! These things ar outside their responsibility. Each class has only one job and
+             * it does it perfectly. We don't want other classes to know about all the details! We hide the details
+             * from certain classes: so we can define certain fields as PRIVATE and then give them PUBLIC getters
+             * and setters to access them when we need them. 
              * */
            // Console.WriteLine();
+
+            var evie = new Person();
+
+            // set the birth date to a value
+            evie.SetBirthdate(new DateTime(1993, 06, 23));
+            // read the birth date
+            Console.WriteLine(evie.GetBirthdate());
             Console.ReadKey();
         }
     }
