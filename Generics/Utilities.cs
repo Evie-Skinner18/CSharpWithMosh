97307fde1c996d94486cb2f2e9d4063be40add15
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-10-11 14:12:11 +0100

    Changed the Utilities class to a gneeric class. Now there is a constraint on the method

--- Generics/Utilities.cs
+++ Generics/Utilities.cs
@@ -1,15 +1,21 @@
-﻿namespace Generics
+﻿using System;
+
+namespace Generics
 {
-    public class Utilities
+    // we've created a generic class
+    public class Utilities<T> where T : IComparable
     {
         public int WhichIsGreater(int a, int b)
         {
             return a > b ? a : b;
         }
 
-        public T WhichIsGreater<T>(Ta, Tb)
+        
+        public T WhichIsGreater(T a, T b)
         {
-
+            // constraint: unless we put IComparable above, the computer doesn't know what Ta or Tb are so can't
+            // evaluate the greater than expression
+            return a.CompareTo(b) > 0 ? a : b;
         }
     }
 }
