f9fca74bc73fd7b193c77606c07872209aeab4ae
Evie-Skinner18 <eskinner@spartaglobal.com>
2018-10-13 18:38:18 +0100

    Started the Classes section on non primitive types

---
+++ UdemyCourses/CSharpBasics/CSharpBasics/Classes/Maths/Calculator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Classes.Maths
+{
+    public class Calculator
+    {
+
+    }
+}
---
+++ UdemyCourses/CSharpBasics/CSharpBasics/Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Classes.Maths;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var evie = new Person();
+            evie.FirstName = "Evie";
+            evie.LastName = "Smellybick";
+            evie.Introduce();
+
+            var newCalculator = new Calculator();
+        }
+    }
+}
