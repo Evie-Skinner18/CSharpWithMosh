b3b4155f43eab76f290d1cbce04558f3509fae8d
Evie-Skinner18 <eskinner@spartaglobal.com>
2018-10-13 17:59:10 +0100

    Added exercises on Non primitive types

--- UdemyCourses/CSharpBasics/CSharpBasics/CSharpBasics/Program.cs
+++ UdemyCourses/CSharpBasics/CSharpBasics/CSharpBasics/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace CSharpBasics
+namespace Variables
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.97f;
+            char firstLetter = 'E';
+            string bitch = "Bitch";
+            //const pi = 3.14f;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(firstLetter);
+            Console.WriteLine("Your mum is a {0}, {1}",bitch, firstLetter);
+            Console.ReadKey();
         }
     }
 }
---
+++ UdemyCourses/CSharpBasics/CSharpBasics/Operators/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+                                                        
+        {
+            var firstNum = 12;
+            var secondNum = 8;
+            Console.WriteLine((float)firstNum / (float)secondNum);
+        }
+    }
+}
