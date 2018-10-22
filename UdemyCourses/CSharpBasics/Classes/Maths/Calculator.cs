728e0d40bd35ffd76dfe7913ee144429329c4026
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-10-22 14:08:46 +0100

    Completed the Classes section of Udemy course

--- UdemyCourses/CSharpBasics/Classes/Maths/Calculator.cs
+++ UdemyCourses/CSharpBasics/Classes/Maths/Calculator.cs
@@ -4,6 +4,9 @@ namespace Classes.Maths
 {
     public class Calculator
     {
-
+        public int Add(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
     }
 }
--- UdemyCourses/CSharpBasics/Classes/Program.cs
+++ UdemyCourses/CSharpBasics/Classes/Program.cs
@@ -13,6 +13,13 @@ namespace Classes
             evie.Introduce();
 
             var newCalculator = new Calculator();
+            var sum = newCalculator.Add(4983, 957);
+
+            // WriteLine is static method so we don't need to make a new Console object each time
+            // we call this
+            Console.WriteLine("The sum is {0}", sum);
+           
+            Console.ReadKey();
         }
     }
 }
