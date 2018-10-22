6df66545872e4c1a5495aa9418b74e69226524f2
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-10-22 14:38:14 +0100

    Added Arrays project

---
+++ UdemyCourses/CSharpBasics/Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3];
+            numbers[0] = 1;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            Console.ReadKey();
+        }
+    }
+}
