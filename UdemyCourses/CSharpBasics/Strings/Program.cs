2a30293a1e2b83b984f3c40e304a411017e08809
Evie-Skinner18 <eskinner@spartaglobal.com>
2018-10-30 14:45:27 +0000

    Started the strings tutorial

---
+++ UdemyCourses/CSharpBasics/Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Jimbo";
+            string lastName = "Da Formagge";
+
+            var fullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            string [] names = {"Evie", "Nelly", "Jimbo", "Mobwicks"};
+            var formattedNames = string.Join(" and ", names);
+            Console.WriteLine(fullName);
+            Console.WriteLine(formattedNames);
+        }
+    }
+}
