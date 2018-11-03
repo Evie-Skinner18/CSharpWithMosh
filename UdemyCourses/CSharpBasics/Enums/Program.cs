ce0dc2e8af09aee2919f2aa2e00009e4d48a04e0
Evie-Skinner18 <eskinner@spartaglobal.com>
2018-11-03 15:10:49 +0000

    Started ref types and value types demo

--- UdemyCourses/CSharpBasics/Enums/Program.cs
+++ UdemyCourses/CSharpBasics/Enums/Program.cs
@@ -5,9 +5,9 @@ namespace Enums
     // enum is new type so need to define at the namespace level
     public enum ShippingMethod
     {
-        RegularAirMail,
-        RegisteredAirMail,
-        Express
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
     }
 
     class Program
@@ -16,7 +16,23 @@ namespace Enums
         {
             // an enum is an int that can have different values. Instead of declaring three different consts, we can
             // declare one enum that has three different versions.
-            Console.WriteLine("Hello World!");
+            var method = ShippingMethod.Express;
+
+            // want to see the actual value of the Express member so we do a typecast to int
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            // want to see which member's value matches to three so we do a typecast to ShippingMethod
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method);
+
+            // convert a string to an enum in two different ways
+            string methodName = "Express";
+            var parsedShippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine(parsedShippingMethod);
+
+            var myShippingMethod = Enum.Parse<ShippingMethod>(methodName);
         }
     }
 }
---
+++ UdemyCourses/CSharpBasics/ReferenceTypesAndValueTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReferenceTypesAndValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
