f4855d26dcc130b0d8800cbe4668f2ee7a1d4f8a
Evie-Skinner18 <eskinner@spartaglobal.com>
2018-11-03 14:51:28 +0000

    Started Enums demo

---
+++ UdemyCourses/CSharpBasics/Enums/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Enums
+{
+    // enum is new type so need to define at the namespace level
+    public enum ShippingMethod
+    {
+        RegularAirMail,
+        RegisteredAirMail,
+        Express
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // an enum is an int that can have different values. Instead of declaring three different consts, we can
+            // declare one enum that has three different versions.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
