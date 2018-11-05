861806ce66ce4d1e42fb3d5e1b39ac2932b6ca41
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-11-05 17:27:27 +0000

    Completed for loops and foreach loops

---
+++ UdemyCourses/CSharpBasics/ForLoops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+
+            for (int i = 10; i >= 1; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
