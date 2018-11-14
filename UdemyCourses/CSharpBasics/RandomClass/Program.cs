4e87966ef4b8258a409d4a3fbaafef13b15afb3a
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-11-14 18:06:53 +0000

    Finished while loops and started Random Class

---
+++ UdemyCourses/CSharpBasics/RandomClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RandomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(random.Next(1, 10));
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UdemyCourses/CSharpBasics/WhileLoops/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            // if you don't know how many times the loop will iterate , it's best to use
+            // while. for is better for a discrete number
+            while (true)
+            {
+                Console.Write( "type your name: ");
+                var userInput = Console.ReadLine();
+
+                // this string method checks fornull or nothing and returns bool. So when
+                // the user presses enter key it will exit!
+                if (!String.IsNullOrWhiteSpace(userInput))
+                {
+                    Console.WriteLine("@Echo: " + userInput);
+                    // cont means go back to beginning of the loop
+                    continue;
+                }
+
+                Console.WriteLine("byebye!");
+                break;
+                
+                
+            }
+        }
+    }
+}
