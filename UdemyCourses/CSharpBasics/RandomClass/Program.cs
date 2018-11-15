afd3f9e1074e7b3faae0157e88bc4200edbfda92
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-11-15 12:03:08 +0000

    Making a random password

--- UdemyCourses/CSharpBasics/RandomClass/Program.cs
+++ UdemyCourses/CSharpBasics/RandomClass/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace RandomClass
 {
@@ -7,13 +8,23 @@ namespace RandomClass
         static void Main(string[] args)
         {
             var random = new Random();
+            var buffer = new char[10];
+
+
 
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(random.Next(1, 10));
-                
+                // the console will show ten random ASCII chars whose numbers are between 0 and 26 after
+                // the char 'a'. this equates to alphabet! Make the range of numbers wider for alphanumeric
+                // . Make a random password
+                buffer[i] = (char)('a' + random.Next(0, 50));
             }
+            // you can create a new string by passig in an array of chars as argument
+            var password = new string(buffer);
+            Console.WriteLine(password);
 
+            // 97 represents the char a in a computer. ASCII
+            // Console.WriteLine((int) 'a');
             Console.ReadKey();
         }
     }
