9bc93c2fdbf90488b974503841fa60dc481713fe
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-11-19 17:50:45 +0000

    Trying to do the factorials exercise but its really hard

---
+++ UdemyCourses/CSharpBasics/Factorials/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Factorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Factorial-ifier! Please enter a number...");
+            var userInput = Int32.Parse(Console.ReadLine());
+            int result;
+
+            for (int i = 0; i < userInput; i++)
+            {
+                result = userInput * (userInput - i);
+               
+            }
+            Console.WriteLine(userInput + "! = " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UdemyCourses/CSharpBasics/GuessingGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
