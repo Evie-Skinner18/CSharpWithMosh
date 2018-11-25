b423cbb2b197f7ffcd8b39937aae4024ff1b249e
Evie-Skinner18 <eskinner@spartaglobal.com>
2018-11-25 18:47:21 +0000

    Completed the max value exercise. At some point need to go back and improve the ones I didnt manage to do

---
+++ UdemyCourses/CSharpBasics/CalcMaxInt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CalcMaxInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! I want to do some number crunching. Please can you give me a list " +
+                              "of integers separated by commas to crunch on?");
+
+            var userInput = Console.ReadLine();
+
+            var stringNums = userInput.Split(',', userInput.Length);
+            var max = 0;
+
+            foreach (var stringNum in stringNums)
+            {
+                int num = Int32.Parse(stringNum);
+                max = num > max ? num : max;
+            }
+            Console.WriteLine("The maximum of your values is " + max);
+        }
+    }
+}
