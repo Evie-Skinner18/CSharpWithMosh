19d739f2018bc550da7309d5644eb142a31cdf73
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-11-27 17:42:21 +0000

    Finished multidimensional arrays

--- UdemyCourses/CSharpBasics/MultidimensionalArrays/Program.cs
+++ UdemyCourses/CSharpBasics/MultidimensionalArrays/Program.cs
@@ -19,7 +19,7 @@ namespace MultidimensionalArrays
             // should display 77
             //Console.WriteLine(matrix[0,2]);
 
-            var numbers = new[] {1,60,88,104,25};
+            var numbers = new[] {60,88,1,104,25};
 
             // Length
             Console.WriteLine("The length of this array is " + numbers.Length);
@@ -32,15 +32,45 @@ namespace MultidimensionalArrays
             // Turns a specified amount of items in the array into default value (so 0 for ints)
             // at the specified index. Here, final item in the array will display as 0 instead of 25
             Array.Clear(numbers, 4, 1);
+            Console.WriteLine( "the effect of Clear()");
             foreach (var num in numbers)
             {
                 Console.WriteLine(num);
             }
 
             // copy
+            // copies the elements in one array and pastes them into another. Here we initialise
+            // an empty array of 3items so first three from numbers will copy over to anotherArray
+            var anotherArray = new int[3];
+            Array.Copy(numbers, anotherArray,3);
+            Console.WriteLine("the effect of Copy()");
+            foreach (var n in anotherArray)
+            {
+                Console.WriteLine(n);
 
+            }
 
+            // sort
+            // sorts the items into numerical order
+            Array.Sort(numbers);
+            Console.WriteLine("the effect of Sort()");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine( n);
+            }
 
+            // reverse
+            // reverses the order of the elements.
+            Array.Reverse(numbers);
+            Console.WriteLine("the effect of Reverse()");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            // all these methods actually modify the original array
+            Console.ReadKey();
+      
         }
     }
 }
