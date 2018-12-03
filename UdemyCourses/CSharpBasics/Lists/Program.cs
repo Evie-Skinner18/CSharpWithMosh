3eae491cfaec9fdc175bde3e489c3a2888cff460
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-12-03 17:37:31 +0000

    Adde more about lists

--- UdemyCourses/CSharpBasics/Lists/Program.cs
+++ UdemyCourses/CSharpBasics/Lists/Program.cs
@@ -7,10 +7,39 @@ namespace Lists
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
             // Lists are dynamic in length unlike arrays. List is a gneeric
 
-            var nums = new List<>
+            // you can add as many objects as you like and the contianer will grow to fit these things inside
+            var nums = new List<int>()
+            {
+                1,
+                2,
+                3,
+                4
+            };
+            nums.Add(1);
+
+            // add another Enumerable to the list. So can be an array or another list!
+            nums.AddRange(new int[3] {5, 6, 7});
+
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine($"The index of the first 1 I've found in nums is {nums.IndexOf(1)}.");
+            Console.WriteLine($"The index of the last 1 in nums is {nums.LastIndexOf(1)}.");
+
+            Console.WriteLine($"I've counted the number of items in nums: {nums.Count}");
+
+            // this will remove the first 1 it finds
+            nums.Remove(1);
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+
+            }
+            Console.ReadKey();
 
         }
     }
