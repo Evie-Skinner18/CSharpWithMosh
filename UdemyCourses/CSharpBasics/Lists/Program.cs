9b23261c7496052fbeadda31b55bd1b2ce7c653b
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-12-03 17:21:48 +0000

    Started Lists

---
+++ UdemyCourses/CSharpBasics/Lists/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            // Lists are dynamic in length unlike arrays. List is a gneeric
+
+            var nums = new List<>
+
+        }
+    }
+}
