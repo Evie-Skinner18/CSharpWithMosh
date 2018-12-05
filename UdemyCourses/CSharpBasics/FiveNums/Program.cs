e2184f1c96ff16541f0eee5fa98f9f25f18609d1
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-12-05 17:01:31 +0000

    Working on five nums challenge. How do I check if there are duplicate nums in the list?

---
+++ UdemyCourses/CSharpBasics/FiveNums/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FiveNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello number friend! Let's play number wang! Please give me " +
+                              "five numbers.");
+            var userInputChars = Console.ReadLine().ToCharArray();
+            var userInputNums = new List<int>();
+            int num;
+
+            foreach (var ch in userInputChars)
+            {
+                num = Int32.Parse(ch.ToString());
+                userInputNums.Add(num);
+            }
+
+            foreach (var listNum in userInputNums)
+            {
+                if (listNum == )
+                {
+                    
+                }
+                Console.WriteLine(listNum);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
