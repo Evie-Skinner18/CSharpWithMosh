be90e6a74e2bec2709c72e7c2cfded3176ee7ddf
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-12-05 17:15:59 +0000

    Prob with logic in for loop to check for duplicate nums

--- UdemyCourses/CSharpBasics/FiveNums/Program.cs
+++ UdemyCourses/CSharpBasics/FiveNums/Program.cs
@@ -9,6 +9,8 @@ namespace FiveNums
         {
             Console.WriteLine("Hello number friend! Let's play number wang! Please give me " +
                               "five numbers.");
+
+            // need a while loop
             var userInputChars = Console.ReadLine().ToCharArray();
             var userInputNums = new List<int>();
             int num;
@@ -19,14 +21,25 @@ namespace FiveNums
                 userInputNums.Add(num);
             }
 
-            foreach (var listNum in userInputNums)
+            for (var i = 0; i < userInputNums.Count; i++)
             {
-                if (listNum == )
-                {
-                    
-                }
-                Console.WriteLine(listNum);
+                // this don't work
+                //if (userInputNums[i] == userInputNums[i - 1])
+                //{
+                //    Console.WriteLine($"Soz you've already entered {userInputNums[i]}. Please try another set of " +
+                //                      $"five numbers.");
+                //}
+                //Console.WriteLine(listNum);
             }
+
+            Console.WriteLine("Thant's numberwang! Your sorted list of numbers is: ");
+
+            userInputNums.Sort();
+            foreach (var number in userInputNums)
+            {
+                Console.WriteLine(number);
+            }
+
 
             Console.ReadKey();
 
