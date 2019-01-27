133bb6ef637e0110e3b5a69bd16a492a37228b61
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-01-27 16:28:56 +0000

    Challenge nearly complete. Having fun with it by making a class that has methods to generate cutting remarks about the users social media life

--- UdemyCourses/CSharpBasics/FacebookLikes/Program.cs
+++ UdemyCourses/CSharpBasics/FacebookLikes/Program.cs
@@ -7,16 +7,41 @@ namespace FacebookLikes
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello slave of Facey B. I am your friend. Let's see how many friends are pretending " +
+                                    "to like your pathetic posts.");
+            Console.WriteLine("Type something inane to continue our journey, or press the enter key " +
+                              "at any time to leave...");
+
             var userInput = Console.ReadLine().ToLower();
             var friendsWhoLike = new List<string>();
 
-            while (!String.IsNullOrWhiteSpace(userInput))
+            if (!String.IsNullOrWhiteSpace(userInput))
             {
-                Console.WriteLine("Hello slave of Facey B. I am your friend. Let's see how many friends are pretending" +
-                                  "to like your pathetic posts.");
+                Console.WriteLine("Type in a name of a friend ooh friend! Keep typing in names and then press Enter" +
+                                  "to see how many of them are feigning interest in your life.");
+                var friend = Console.ReadLine();
+                while (!String.IsNullOrWhiteSpace(friend))
+                {
+                    friendsWhoLike.Add(friend);
+
+                    Console.WriteLine("Type in another name of a friend ooh friend!");
+                    friend = Console.ReadLine();
+                }
             }
 
+            var restOfFriends = friendsWhoLike.Count - 2;
+            var sarkyMessage = friendsWhoLike.Count > 2 ? $"{friendsWhoLike[0]}, {friendsWhoLike[1]} and {restOfFriends}" +
+                                             " like your "
 
+
+
+            Console.WriteLine("Bye dickhead!");
+
+
+
+        }
+        public class SarkyMessageGenerator()
+        {
 
         }
     }
