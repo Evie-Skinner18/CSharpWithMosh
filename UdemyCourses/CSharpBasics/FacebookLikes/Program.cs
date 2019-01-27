b44d278e81555de24b0af8e4818cd562e2aa3674
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-01-27 15:45:43 +0000

    Started the FB challenge

---
+++ UdemyCourses/CSharpBasics/FacebookLikes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FacebookLikes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userInput = Console.ReadLine().ToLower();
+            var friendsWhoLike = new List<string>();
+
+            while (!String.IsNullOrWhiteSpace(userInput))
+            {
+                Console.WriteLine("Hello slave of Facey B. I am your friend. Let's see how many friends are pretending" +
+                                  "to like your pathetic posts.");
+            }
+
+
+
+        }
+    }
+}
