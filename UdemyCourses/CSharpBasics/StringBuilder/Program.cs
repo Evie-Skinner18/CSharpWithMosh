448d3cf7ba6a9508822e6f6d2a36699cb68fc4c3
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-02-11 18:30:17 +0000

    Finished the string methods and text summariser lectures

--- UdemyCourses/CSharpBasics/StringBuilder/Program.cs
+++ UdemyCourses/CSharpBasics/StringBuilder/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace StringBuilder
+namespace StringMethods
 {
     class Program
     {
@@ -15,6 +15,28 @@ namespace StringBuilder
             Console.WriteLine($"Substring: {fullName.Substring(0, index)}");
             Console.WriteLine($"Split: {names[0]}");
             Console.WriteLine($"Split: {names[1]}");
+
+            Console.WriteLine($"Replace: {fullName.Replace("Evie", "Eveline")}");
+            Console.WriteLine($"Replace: {fullName.Replace("e", "BITCH")}");
+            Console.WriteLine($"Replace: {fullName.Replace("i", "£")}");
+
+            // nul or empty will count a space as a valid string so it's not always reliable
+            // you can count on null or whitespace!
+            if(String.IsNullOrEmpty(" "))
+                Console.WriteLine( "This is invalid soz!");
+            if(String.IsNullOrWhiteSpace(" "))
+                Console.WriteLine("This is invalid soz!");
+
+            var str = "25";
+            var age = Convert.ToByte(str);
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            var realPrice = price.ToString("C0");
+            // how does it get the £ sign?
+            Console.WriteLine(realPrice);
+
+            Console.ReadKey();
         }
     }
 }
