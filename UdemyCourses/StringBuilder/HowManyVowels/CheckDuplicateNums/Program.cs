18e5f2c049c1f2439a21339cf150a30e7cdc15ff
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-03-03 14:17:08 +0000

    Started duplicate nums challenge

---
+++ UdemyCourses/StringBuilder/HowManyVowels/CheckDuplicateNums/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CheckDuplicateNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey girlfrenn. Type in some numbers separated by hyphens and I'll check for any duplicates!");
+            var userInput = Console.ReadLine().Split('-').ToList();
+
+            foreach (var num in userInput)
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+}
