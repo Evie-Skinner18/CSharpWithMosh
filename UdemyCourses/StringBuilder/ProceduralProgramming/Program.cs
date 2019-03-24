8fbb8a24b1856cc03be34db3753b76b61a51afb0
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-03-24 14:32:26 +0000

    Started procedural programming

---
+++ UdemyCourses/StringBuilder/ProceduralProgramming/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProceduralProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // you should always separate the logic from the bits that log to the console
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
