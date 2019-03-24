7b89e66175f020fab242a13d229d639dd14ff459
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-03-24 14:42:29 +0000

    Refactored the reverse name challenge so it happens in a separate function

--- UdemyCourses/StringBuilder/ProceduralProgramming/Program.cs
+++ UdemyCourses/StringBuilder/ProceduralProgramming/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace ProceduralProgramming
 {
@@ -7,7 +9,26 @@ namespace ProceduralProgramming
         static void Main(string[] args)
         {
             // you should always separate the logic from the bits that log to the console
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the name reversor! Please enter your enam.. I mean name...");
+            var userName = Console.ReadLine();
+            var reversedName = ReverseName(userName);
+
+            Console.WriteLine($"Your new name is {reversedName}. Enjoy!");
+
+
+            Console.ReadKey();
+        }
+
+        public static string ReverseName(string userName)
+        {
+            var reversedNameChars = userName.Reverse();
+            var reversedNameStr = new StringBuilder();
+
+            foreach (var ch in reversedNameChars)
+            {
+                reversedNameStr.Append(ch);
+            }
+            return reversedNameStr.ToString();
         }
     }
 }
