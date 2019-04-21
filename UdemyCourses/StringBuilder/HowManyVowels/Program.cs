0546dd5d4c0ecf31d6f5e5aaf2442dc3525d5d45
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-04-21 12:27:35 +0100

    For some reason the HowManyVowels challenge had parts from the other challenges in Excersises in it. Deleted these and I think that has deleted them from the projects they belonged too. Oops oh well I was getting confused anyway because we need to move on to objects

--- UdemyCourses/StringBuilder/HowManyVowels/Program.cs
+++ UdemyCourses/StringBuilder/HowManyVowels/Program.cs
@@ -11,11 +11,18 @@ namespace HowManyVowels
             Console.WriteLine("Hello there, welcome to the vowel-o-matic! Please enter a word and I will count the number " +
                 "of vowels for you");
             var userInput = Console.ReadLine().ToLower();
-            var vowelCount = 0;
-            vowelCount = userInput.Count(c => c.Equals('a') || c.Equals('e') || c.Equals('i') || c.Equals('o')
-                                                 || c.Equals('u'));
+            var vowelCount = GetVowelCount(userInput);
+
+
             Console.WriteLine($"The number of vowels in {userInput} is {vowelCount}. Seeya soon!");
             Console.ReadKey();
+        }
+
+        public static int GetVowelCount(string userInput)
+        {
+            return userInput.Count(c => c.Equals('a') || c.Equals('e') || c.Equals('i') || c.Equals('o')
+                                     || c.Equals('u'));
+
         }
     }
 }
