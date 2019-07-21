ca9c0c65aef39d688297b971a122525520291aa3
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-07-21 16:37:42 +0100

    Added intermediate course project

---
+++ UdemyCourses/CSharpIntermediate/IntermediateCourse/IntermediateCourse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntermediateCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
