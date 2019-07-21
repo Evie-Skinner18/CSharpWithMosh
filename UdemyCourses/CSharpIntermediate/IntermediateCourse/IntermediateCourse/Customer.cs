970384c5d0edde2855158c48fd788bfdebacc882
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-07-21 19:03:53 +0100

    Lesson is, only use a ctor if you really need one. Dont use many different ctors as it gets ugly

--- UdemyCourses/CSharpIntermediate/IntermediateCourse/IntermediateCourse/Customer.cs
+++ UdemyCourses/CSharpIntermediate/IntermediateCourse/IntermediateCourse/Customer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace IntermediateCourse
 {
     public class Customer
@@ -6,11 +8,27 @@ namespace IntermediateCourse
 
         public int Id;
         public string Name;
+        public List<Order> Orders;
 
+        // CTOR 1 default ctor
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
 
-        public Customer(int id, string name)
+        //CTOR 2 another ctor. Using :this() ensures that default ctor will be called before CTOR2 
+        public Customer(int id)
+            :this()
         {
             this.Id = id;
+        }
+
+        // CTOR 3 and another one! When we use CTOR 3, it ensures that CTOR 2 will be called before
+        // CTOR 3. CTOR 2 ensures that the default one CTOR 1 will be called before CTOR 2 so all of them will
+        // be called
+        public Customer(int id, string name)
+            : this(id)
+        {
             this.Name = name;
         }
     }
