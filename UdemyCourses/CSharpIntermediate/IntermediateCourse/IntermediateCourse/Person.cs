e6b8018fccc4e276676dff995e8fa5afd4f5f8f5
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-07-21 18:04:30 +0100

    Made a Person class with two methods

---
+++ UdemyCourses/CSharpIntermediate/IntermediateCourse/IntermediateCourse/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace IntermediateCourse
+{
+    public class Person
+    {
+        public string Name;
+
+        public Person()
+        {
+        }
+
+        public void Introduce(string greetersName)
+        {
+            Console.WriteLine("Hello {0}, I am {1}", greetersName, Name);
+        }
+
+        public static Person Parse(string word)
+        {
+            var anotherPerson = new Person
+            {
+                Name = word
+            };
+
+            return anotherPerson;
+        }
+    }
+}
--- UdemyCourses/CSharpIntermediate/IntermediateCourse/IntermediateCourse/Program.cs
+++ UdemyCourses/CSharpIntermediate/IntermediateCourse/IntermediateCourse/Program.cs
@@ -2,11 +2,23 @@
 
 namespace IntermediateCourse
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //var firstPerson = new Person
+            //{
+            //    Name = "bitch"
+            //};
+
+            //firstPerson.Introduce("Evie");
+
+            // parse method is static so you can make a person without having to instatiate a person
+            var anotherPerson = Person.Parse("bitch");
+            anotherPerson.Introduce("Evie");
+
         }
     }
 }
