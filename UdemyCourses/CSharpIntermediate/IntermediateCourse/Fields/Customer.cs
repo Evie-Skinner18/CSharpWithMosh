53aa1356e313326226c7d54f45ce016b78384613
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-08-04 19:01:05 +0100

    Added a customer class with some properties and a Promote() method

---
+++ UdemyCourses/CSharpIntermediate/IntermediateCourse/Fields/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+
+        //default ctor initialises empty list of orders
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        // this ctor 3 will call ctor 2 in order to get the id, and then take care of name itself
+        public Customer(int id, string name)
+            :this(id)
+        {
+            this.Name = name;
+        }
+
+        // they can get promoted to gold customer!
+        public void Promote()
+        {
+            // you already have a list of orders initliaised by default
+            // in the Customer ctor. what if you accidentally re-initialise it?
+
+        }
+    }
+}
---
+++ UdemyCourses/CSharpIntermediate/IntermediateCourse/Fields/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello customers!");
+
+            var customer = new Customer();
+            
+        }
+    }
+}
