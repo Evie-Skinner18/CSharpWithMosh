a9bce90fe8fa386f709e78addc8009d8ea2a2778
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-08-15 18:26:01 +0100

    Added a private setter for bday and a getter for Age

--- UdemyCourses/CSharpIntermediate/IntermediateCourse/AccessModifiers/Person.cs
+++ UdemyCourses/CSharpIntermediate/IntermediateCourse/AccessModifiers/Person.cs
@@ -4,20 +4,47 @@ namespace AccessModifiers
 {
     public class Person
     {
-        private DateTime _birthday { get; set; }
+        public Person(DateTime dateTime)
+        {
+            Birthday = Birthday;
+        }
+
+        // PROEPRTIES AUTO-IMPLEMENTED WAY: C# compiler internally creates a private field
+        // and implements getter and setter for us
+        // setter is now private so we use dependency injection in the ctor to pass in a bday upon instantiation
+        public DateTime Birthday { get; private set; }
+        public int Age
+        {
+            get
+            {
+                var timespan = DateTime.Today - Birthday;
+                var years = timespan.Days / 365;
+
+                return years;
+            }
+        }
+
+
+        //private DateTime _birthday;
 
         // we are using dependency injection with the setter
-        public void SetBirthday(DateTime birthday)
-        {
-            _birthday = birthday;
-        }
+        //public void SetBirthday(DateTime birthday)
+        //{
+        //    _birthday = birthday;
+        //}
 
         // the getter gets you  the birthdayso even if it's private you can grab it this way
-        public DateTime GetBirthday()
-        {
-            return _birthday;
-        }
+        //public DateTime GetBirthday()
+        //{
+        //    return _birthday;
+        //}
 
 
+        // PROPERTIES long way
+        //public DateTime Birthdate
+        //{
+        //    get { return _birthday; }
+        //    set { _birthday = value; }
+        //}
     }
 }
--- UdemyCourses/CSharpIntermediate/IntermediateCourse/AccessModifiers/Program.cs
+++ UdemyCourses/CSharpIntermediate/IntermediateCourse/AccessModifiers/Program.cs
@@ -14,13 +14,22 @@ namespace AccessModifiers
          */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
-            var evie = new Person();
+            var evie = new Person(new DateTime(1993, 06, 23));
 
             // my bday is special so you can't access it right away. Have to use setter and getter
-            evie.SetBirthday(DateTime.Parse("23/06/1993"));
-            var bestBday = evie.GetBirthday();
+            //evie.SetBirthday(DateTime.Parse("23/06/1993"));
+            //var bestBday = evie.GetBirthday();
+
+            var matureAge = evie.Age;
+
+            Console.WriteLine(matureAge);
+
+            // objects are about behaviour. Technically yeah you can access the _birthday field outside the class
+            // but it's still hiding because you have to do extra work to reveal _birthday
+
+            // you can do the same thing with less code by using PROPERTIES! :->
+
         }
     }
 }
