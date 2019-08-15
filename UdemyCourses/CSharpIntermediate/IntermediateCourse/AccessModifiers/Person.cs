8d4444c8fc941e55ba751ddd1e3ad25be578bce0
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-08-15 18:02:06 +0100

    Added new project AccessModifiers and practising encapsulation within that. MAde a getter and setter for my bday

---
+++ UdemyCourses/CSharpIntermediate/IntermediateCourse/AccessModifiers/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        private DateTime _birthday { get; set; }
+
+        // we are using dependency injection with the setter
+        public void SetBirthday(DateTime birthday)
+        {
+            _birthday = birthday;
+        }
+
+        // the getter gets you  the birthdayso even if it's private you can grab it this way
+        public DateTime GetBirthday()
+        {
+            return _birthday;
+        }
+
+
+    }
+}
---
+++ UdemyCourses/CSharpIntermediate/IntermediateCourse/AccessModifiers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+
+        /* OOP PRINCIPLE: ENCAPSULATION
+         * 
+         * you declare certain fields as private and then use getters and setters as public
+         * in order to grab them
+         * 
+         * 
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var evie = new Person();
+
+            // my bday is special so you can't access it right away. Have to use setter and getter
+            evie.SetBirthday(DateTime.Parse("23/06/1993"));
+            var bestBday = evie.GetBirthday();
+        }
+    }
+}
