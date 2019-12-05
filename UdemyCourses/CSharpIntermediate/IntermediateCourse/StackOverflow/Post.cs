bf05b8fb183924250650d74fceec6c3f0831fa54
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-12-05 17:38:28 +0000

    Started the stack overflow challenge

---
+++ UdemyCourses/CSharpIntermediate/IntermediateCourse/StackOverflow/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StackOverflow
+{
+    public class Post
+    {
+        private string _title { get; set; }
+        private string _description { get; set; }
+        private DateTime _dateCreated { get; set; }
+        protected int VoteValue { get; set; }
+
+
+        public void UpVotePost()
+        {
+            VoteValue += VoteValue;
+        }
+
+        public void DownVotePost()
+        {
+            VoteValue -= VoteValue;
+        }
+
+
+    }
+}
