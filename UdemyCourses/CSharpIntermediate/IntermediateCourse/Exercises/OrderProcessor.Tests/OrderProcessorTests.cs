1cdd53d340ad9a2baba5db2d45bd5b3c6ee186a5
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-12-17 11:38:26 +0000

    Started improving my tests to make them more legible for humans. E.g use Assert.That(thisThing, Is.EqualTo(true)) etc

--- UdemyCourses/CSharpIntermediate/IntermediateCourse/Exercises/OrderProcessor.Tests/OrderProcessorTests.cs
+++ UdemyCourses/CSharpIntermediate/IntermediateCourse/Exercises/OrderProcessor.Tests/OrderProcessorTests.cs
@@ -16,16 +16,27 @@ namespace OrderProcessorTests
             _orderProcessor = new OrderProcessor(_shippingCalculator);
         }
 
-        
+        // negative use case
         [Test]
         public void CannotProcessShippedOrder_ShouldReturnInvalidOperationException()
         {
             var order = new Order(){TotalPrice = 12f, IsShipped = true};
-           //_orderProcessor.ProcessOrder(order);
 
             Assert.That(order, Is.Not.Null);
+            Assert.That(order.IsShipped, Is.True);
             // pretty sure that's an Nunit lambda expression
             Assert.Throws<InvalidOperationException>(() => _orderProcessor.ProcessOrder(order));
+        }
+
+        [Test]
+        public void CanCalculateShipping_ShouldReturn1f()
+        {
+            var order = new Order() { TotalPrice = 100f, IsShipped = false };
+            var fakeCalculatorsShippingCost = _shippingCalculator.CalculateShipping(order);
+
+            Assert.That(fakeCalculatorsShippingCost, Is.Not.Null);
+            Assert.That(order.IsShipped, Is.False);
+            Assert.That(fakeCalculatorsShippingCost, Is.EqualTo(1f));
         }
     }
 }
