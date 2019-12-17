60336c5a3cb1a1dabc299aea14cb5eed98fe55ff
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-12-17 18:21:09 +0000

    Finished the challenge. This sln to the problem has an interface that demonstrates polymorphic behaviour. It also shows Open Closed Principle because I don't have to change Workflow Engine in order to make it do diferent things. To-do: test the WorkflowEngine by making a fake activity

--- UdemyCourses/CSharpIntermediate/IntermediateCourse/WorkflowEngine/Program.cs
+++ UdemyCourses/CSharpIntermediate/IntermediateCourse/WorkflowEngine/Program.cs
@@ -6,7 +6,24 @@ namespace WorkflowEngine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello and welcome to Evie's Workflow Engine!");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Hello and welcome to Evie's Workflow Engine! Let's have a weekend of fun activities.");
+
+            var drummingActivity = new MusicalActivity();
+            var scoffinActivity = new EatingActivity();
+            var ultimateRelaxationActivity = new ChillinActivity();
+
+            var workflowEngine = new WorkflowEngine();
+            workflowEngine.RegisterActivity(drummingActivity);
+            workflowEngine.RegisterActivity(scoffinActivity);
+            workflowEngine.RegisterActivity(ultimateRelaxationActivity);
+
+            Console.WriteLine(workflowEngine.Run());
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("F$OCIETY YEAHHHH WE GONNA GET U BITCH");
         }
     }
 }
--- UdemyCourses/CSharpIntermediate/IntermediateCourse/WorkflowEngine/WorkflowEngine.cs
+++ UdemyCourses/CSharpIntermediate/IntermediateCourse/WorkflowEngine/WorkflowEngine.cs
@@ -1,10 +1,30 @@
-﻿namespace WorkflowEngine
+﻿using System.Collections.Generic;
+
+namespace WorkflowEngine
 {
     public class WorkflowEngine
     {
-        public string Run(Workflow workflow)
-        {
+        private IList<IActivity> _activities { get; set; }
 
+        public WorkflowEngine()
+        {
+            _activities = new List<IActivity>();
+        }
+
+        // list of activities is private so we use public method to add activities from outside
+        public void RegisterActivity(IActivity activity)
+        {
+            _activities.Add(activity);
+        }
+
+        public string Run()
+        {
+            var allExecutionMessages = "";
+
+            foreach (var activity in _activities)
+                allExecutionMessages = $"{allExecutionMessages}\n {activity.Execute()}";
+
+            return allExecutionMessages;            
         }
     }
 }
